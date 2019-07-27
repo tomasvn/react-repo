931b24cb3bbfd23c9f35d4063dc5ef24ed2d91c3
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2019-07-27 12:33:13 +0200

    Fix typo in redux thunk action

--- redux-movie-discover/src/fetch.js
+++ redux-movie-discover/src/fetch.js
@@ -8,9 +8,9 @@ import { fetchStart, fetchSucess, fetchFailed } from './actions'
 
 const api = () => `https://api.themoviedb.org/3/discover/movie?api_key=a0ed6f7f6d5a52eedd05ecfa82be5478`
 
-export const fetchData = async dispatch => {
+export const fetchData = () => async dispatch => {
   dispatch(fetchStart())
-    
+
   try {
     const req = await fetch(api())
     const res = await req.json()
