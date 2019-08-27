d814a60843a354db60fe412b60eda4fa564125f3
tomasvn <nguyenngoct2112@gmail.com>
2019-08-27 11:23:00 +0200

    Remove code argument from url function

--- redux-weather-app/src/helpers/api.js
+++ redux-weather-app/src/helpers/api.js
@@ -1,3 +1,3 @@
 import { key } from './key'
 
-export const url = (city, code) => `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
+export const url = city => `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
